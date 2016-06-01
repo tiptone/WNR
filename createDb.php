e38bde691eb08bfa718fcd0e0a7c6548023de018
tiptone <tiptone@gmail.com>
2016-06-01 13:13:15 -0700

    - working around a jQuery bug with autocomplete - ignoring google site verification files - added a script to generate a db from the origin .csv

---
+++ createDb.php
@@ -0,0 +1,46 @@
+<?php
+
+$row = 1;
+
+$db = new SQLite3('reviews.db');
+
+$sql = "CREATE TABLE reviews (
+  timestamp text,
+  whiskey text,
+  reviewer text,
+  url text,
+  rating integer,
+  region text,
+  price text,
+  date text
+)";
+
+$db->exec($sql);
+
+$sql = "insert into reviews values (:timestamp, :whiskey, :reviewer, :url, :rating, :region, :price, :date)";
+
+$stmt = $db->prepare($sql);
+
+if (($handle = fopen("reviews.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",", '"')) !== FALSE) {
+        //$num = count($data);
+        //echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+		
+		$stmt->bindValue(':timestamp', trim($data[0]));
+		$stmt->bindValue(':whiskey', trim($data[1]));
+		$stmt->bindValue(':reviewer', trim($data[2]));
+		$stmt->bindValue(':url', trim($data[3]));
+		$stmt->bindValue(':rating', trim($data[4]));
+		$stmt->bindValue(':region', trim($data[5]));
+		$stmt->bindValue(':price', trim($data[6]));
+		$stmt->bindValue(':date', trim($data[7]));
+		
+		$stmt->execute();
+		
+		$stmt->clear();
+    }
+    fclose($handle);
+}
+
+echo "Processed $row records" . PHP_EOL;
