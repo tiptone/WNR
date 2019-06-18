bcc7f60e47f3cfd2598ea067c142d8d401a8b232
Brian Tipton <brian.tipton@shsu.edu>
2019-06-18 09:19:40 -0500

    Automagically fetching a current archive, update readme

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ A simple front end to the Whisky Network Review Archive. Updating the data
 is as simple as exporting a copy of the Google Docs spreadsheet as .csv
 and using it as an input to createDb.php.
 
+Run the code below to fetch a copy of the current archive and create an SQLite3
+database in the file `reviews-YYYY-MM-DD.db`.
+
 ```
-php -f createDb.php -- -i GoogleDocs.csv -o reviews.db
+php -f createDb.php
 ```
--- createDb.php
+++ createDb.php
@@ -1,19 +1,12 @@
 <?php
 
-$options = getopt('i:o:');
+$infile = 'https://docs.google.com/spreadsheets/d/1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o/gviz/tq?tqx=out:csv&sheet=Review+Archive';
 
-if (!isset($options['i']) || !isset($options['o'])) {
-    echo "Error: input file (CSV) and output filename must be supplied." . PHP_EOL;
-    echo "  usage: php reviews.php -- -i reviews.csv -o reviews.db" . PHP_EOL;
-    exit;
-}
-
-$infile  = $options['i'];
-$outfile = $options['o'];
+$today = date('Y-m-d');
 
 $row = 1;
 
-$db = new SQLite3($outfile);
+$db = new SQLite3(sprintf("reviews-%s.db", $today));
 
 $sql = "CREATE TABLE reviews (
   timestamp text,
@@ -32,24 +25,26 @@ $sql = "insert into reviews values (:timestamp, :whiskey, :reviewer, :url, :rati
 
 $stmt = $db->prepare($sql);
 
-if (($handle = fopen($infile, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",", '"')) !== FALSE) {
-        $row++;
-		
-		$stmt->bindValue(':timestamp', trim($data[0]));
-		$stmt->bindValue(':whiskey', trim($data[1]));
-		$stmt->bindValue(':reviewer', trim($data[2]));
-		$stmt->bindValue(':url', trim($data[3]));
-		$stmt->bindValue(':rating', trim($data[4]));
-		$stmt->bindValue(':region', trim($data[5]));
-		$stmt->bindValue(':price', trim($data[6]));
-		$stmt->bindValue(':date', trim($data[7]));
-		
-		$stmt->execute();
-		
-		$stmt->clear();
-    }
-    fclose($handle);
+$lines = file($infile);
+array_shift($lines);
+
+foreach ($lines as $line) {
+    $data = str_getcsv($line);
+    $row++;
+
+    $stmt->bindValue(':timestamp', trim($data[0]));
+    $stmt->bindValue(':whiskey', trim($data[1]));
+    $stmt->bindValue(':reviewer', trim($data[2]));
+    $stmt->bindValue(':url', trim($data[3]));
+    $stmt->bindValue(':rating', trim($data[4]));
+    $stmt->bindValue(':region', trim($data[5]));
+    $stmt->bindValue(':price', trim($data[6]));
+    $stmt->bindValue(':date', trim($data[7]));
+
+    $stmt->execute();
+
+    $stmt->clear();
 }
 
 echo "Processed $row records" . PHP_EOL;
+
